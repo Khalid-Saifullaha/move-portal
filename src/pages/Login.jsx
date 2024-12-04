2583d3a99e39dbb710c81b30290b8b269622c3df
Your Name <khalidsaifullaha3@gmail.com>
2024-12-04 18:47:33 +0600

    login and register from added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { FaEye, FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    userLogin(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        navigate(location?.state?.from || "/");
+      })
+      .catch((err) => {
+        setError({ ...error, login: err.code });
+      });
+  };
+
+  const [showPassword, setShowPassword] = useState(false);
+  const emailRef = useRef();
+  const [error, setError] = useState({});
+
   return (
-    <div>
-      <p>Login</p>
+    <div className="flex justify-center">
+      <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-2xl font-semibold text-center">
+          Login to your account
+        </h2>
+        <form onSubmit={handleSubmit} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              ref={emailRef}
+              placeholder="Enter your email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control relative">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="input input-bordered"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="btn btn-xs absolute right-2 top-12"
+            >
+              {showPassword ? <FaRegEyeSlash /> : <FaEye />}
+            </button>
+            {error.login && (
+              <label className="label text-sm text-red-600">
+                {error.login}
+              </label>
+            )}
+            <label className="label">
+              <span className="label-text-alt link link-hover">
+                Forgot password?
+              </span>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">
+          Don’t Have An Account?{" "}
+          <Link to="/register" className="text-red-500">
+            Register
+          </Link>
+        </p>
+        <button className="btn btn-primary text-white rounded-3xl mt-5 flex items-center justify-center">
+          <FcGoogle className="text-2xl mr-2" /> Login with Google
+        </button>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,153 @@
-import React from "react";
+import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [error, setError] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // get form data
+    const form = new FormData(e.target);
+    const name = form.get("name");
+    if (name.length < 5) {
+      setError({ ...error, name: "must be more the 5 character long" });
+      return;
+    }
+    const photo = form.get("photo");
+    const email = form.get("email");
+    const password = form.get("password");
+    if (password.length < 6) {
+      setError({
+        ...error,
+        name: "Password must be at least 6 characters long.",
+      });
+      return;
+    }
+    if (!/[A-Z]/.test(password)) {
+      setError({
+        ...error,
+        password: "Password must contain at least one uppercase letter.",
+      });
+      return;
+    }
+    if (!/[a-z]/.test(password)) {
+      setError({
+        ...error,
+        password: "Password must contain at least one lowercase letter.",
+      });
+      return;
+    }
+    // console.log(name, email, password, photo);
+
+    createNewUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        updateUserProfile({ displayName: name, photoURL: photo })
+          .then(() => {
+            navigate("/");
+          })
+          .catch((err) => {
+            // console.log(err);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.errorMessage;
+        // console.log(errorCode, errorMessage);
+      });
+  };
+
   return (
-    <div>
-      <p>Register</p>
+    <div className=" flex justify-center my-10">
+      {/* <Helmet>
+        <title>Register</title>
+      </Helmet> */}
+      <div className="card  w-full max-w-lg shrink-0 rounded-none p-10">
+        <h2 className="text-2xl font-semibold text-center">
+          Register your account
+        </h2>
+        <form onSubmit={handleSubmit} className="card-body">
+          {/* name input */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Your Name</span>
+            </label>
+            <input
+              name="name"
+              type="text"
+              placeholder="Enter your name"
+              className="input input-bordered"
+              required
+            />
+            {error.name && (
+              <label className="label text-sm text-red-600">{error.name}</label>
+            )}
+          </div>
+          {/* Photo  */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              name="photo"
+              type="text"
+              placeholder="Photo-URL"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* email input */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          {/* password input */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            {error.password && (
+              <label className="label text-sm text-red-600">
+                {error.password}
+              </label>
+            )}
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-neutral rounded-none">Register</button>
+          </div>
+        </form>
+        <p className="text-center font-semibold">
+          Already have An Account ?{" "}
+          <Link to="/login" className="text-red-500">
+            Login
+          </Link>
+        </p>
+        <button className="btn btn-primary text-white rounded-3xl mt-5 flex items-center justify-center">
+          <FcGoogle className="text-2xl mr-2" /> Register with Google
+        </button>
+      </div>
     </div>
   );
 };
