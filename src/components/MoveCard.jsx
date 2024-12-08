3c91c01b05e26cedb353cf6276ebced61bcea45f
Your Name <khalidsaifullaha3@gmail.com>
2024-12-08 20:23:18 +0600

    card hight change

--- src/components/MoveCard.jsx
+++ src/components/MoveCard.jsx
@@ -5,18 +5,27 @@ const MoveCard = ({ move }) => {
   const { _id, photo, name, genre, duration, release, rating, summary } = move;
   return (
     <div className="">
-      <div className="card bg-base-100 w-96 h-[500px] mx-auto shadow-xl dark:bg-gray-900 dark:text-white">
+      <div className="card pl-1 bg-base-100 w-96 h-[600px] mx-auto shadow-xl dark:bg-gray-900 dark:text-white">
         <figure>
           <img src={photo} alt="Shoes" />
         </figure>
-        <div className="md:ml-3 flex flex-col items-start justify-center">
+        <div className="md:ml-3 ml-1 flex flex-col items-start justify-center">
           <h2 className="card-title text-3xl">{name}</h2>
-          <p className="my-1">{summary}</p>
-          <div className=" flex md:flex-col gap-y-3 text-left">
-            <div className="">Genre: {genre}</div>
-            <div className="">Release: {release}</div>
-            <div className="">Duration: {duration} m</div>
-            <div className="">Rating: {rating}</div>
+          <div className="">
+            <span className="font-semibold ">Rating:</span> {rating}
+          </div>
+          <div className=" flex flex-col gap-y-2 text-left">
+            <div className="">
+              <span className="font-semibold ">Release:</span> {release}
+            </div>
+            <div className="">
+              <span className="font-semibold ">Genre:</span> {genre}
+            </div>
+            <div className="">
+              <span className="font-semibold ">Duration:</span> {duration} min
+            </div>
+
+            <p className="my-1">{summary}</p>
           </div>
           <div>
             <NavLink to={`/details/${_id}`}>
